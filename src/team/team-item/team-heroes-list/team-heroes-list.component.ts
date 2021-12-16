4a23b800303cb4e9b7eba21edb70e6e3c608923a
3rmak <r3al3rmak@gmail.com>
2021-12-16 17:43:58 +0200

    new component

---
+++ src/team/team-item/team-heroes-list/team-heroes-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Team, TeamService } from "../../shared";
+import { Hero, HeroService } from "../../../heroes/shared";
+
+@Component({
+  selector: 'app-team-heroes-list',
+  templateUrl: './team-heroes-list.component.html',
+  styleUrls: ['./team-heroes-list.component.css']
+})
+export class TeamHeroesListComponent implements OnInit {
+  @Input() team: Team | undefined;
+
+  constructor(
+    private teamService: TeamService,
+    private heroService: HeroService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  removeHero(hero: Hero) {
+    const confirmResponse = window.confirm('Are you sure to delete hero from team?');
+    if(!confirmResponse || !this.team) {
+      return;
+    }
+
+    const index = this.team.members?.findIndex((item)=>item.id == hero.id);
+    console.log(index)
+    if (index == undefined || index < 0) {
+      return;
+    }
+    this.team.members?.splice(index, 1);
+    console.log(this.team.members)
+
+    this.teamService.editTeam(this.team.id, { members: this.team.members });
+    this.heroService.updateTeamMembers(hero.id, undefined);
+  }
+}
