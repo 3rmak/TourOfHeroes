b6f37b6b6ccd8238a0464b1ff68be6c58821256f
3rmak <r3al3rmak@gmail.com>
2021-12-20 13:57:19 +0200

    edit only admin

--- src/app/heroes/hero-component/hero.component.ts
+++ src/app/heroes/hero-component/hero.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 
 import { Hero, HeroService } from "../shared";
-import { PreviousPageService } from "../../shared";
+import { PreviousPageService, UserStorageService } from "../../shared";
+import {userRolesEnum} from "../../config";
 
 @Component({
   selector: 'hero-component',
@@ -12,15 +13,22 @@ import { PreviousPageService } from "../../shared";
 export class HeroComponent implements OnInit {
   id: number = 0;
   hero: Hero | undefined;
+  hasAccess: boolean = false;
 
   constructor(
     private heroService: HeroService,
     private activatedRoute: ActivatedRoute,
     private router: Router,
-    private previousPageService: PreviousPageService
+    private previousPageService: PreviousPageService,
+    private userStorageService: UserStorageService
   ) { };
 
   ngOnInit() {
+    const response = this.userStorageService.getFromLocalStorage();
+    if (response && response.role == userRolesEnum.ADMIN) {
+      this.hasAccess = true;
+    }
+
     this.activatedRoute.params.subscribe(params=>this.id=params['userId']);
 
     const hero = this.heroService.getById(this.id);
